baa5de733c858eeec658905573b5e43d20198f00
Momenta <info@momentaapp.com>
2026-05-24 12:39:53 +0300

    instagram photos fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import SessionProvider from "@/components/providers/SessionProvider";
 import { LangProvider } from "@/lib/LangContext";
 import { SectionNavProvider } from "@/lib/SectionNavContext";
 import { InAppBrowserProvider } from "@/lib/InAppBrowserContext";
+import InAppMediaReveal from "@/components/InAppMediaReveal";
 import { isInAppBrowserUA } from "@/lib/inAppBrowser";
 import { getSectionNavVisibility } from "@/lib/data";
 
@@ -47,6 +48,7 @@ export default async function RootLayout({
         <SessionProvider>
           <SectionNavProvider value={sectionNavVisibility}>
             <InAppBrowserProvider value={inAppBrowser}>
+              <InAppMediaReveal />
               <LangProvider>
                 <PublicNavWrapper />
                 <OverflowDebugWrapper />
